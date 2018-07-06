d8391960ef63750a967349a9e535886f5bae7f3d
yi1306c12@w2-712 <yi1306c12@gmail.com>
2018-07-06 12:54:32 +0900

    single servo

---
+++ src/caller_single.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include "khr_ros/server_single.h"
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "single_servo_client");
+  if (argc != 3)
+  {
+    ROS_INFO("usage: single_servo_client ID ROTATE");
+    return 1;
+  }
+
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<khr_ros::single_servo>("single_servo");
+  khr_ros::single_servo srv;
+  srv.id = atoi(argv[1]);
+  srv.rotate = atoi(argv[2]);
+
+  if (client.call(srv))
+  {
+    ROS_INFO("servo: id:%d\trotate:%d",srv.id,srv.rotate);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service single_servo");
+    return 1;
+  }
+
+  return 0;
+}
